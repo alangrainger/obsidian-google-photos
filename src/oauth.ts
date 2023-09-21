b275abacd31ab513643e909cfdcec1567cd07130
Alan Grainger <alan.grainger@gmail.com>
2023-09-21 19:51:11 +0200

    Successful auth

--- src/oauth.ts
+++ src/oauth.ts
@@ -1,19 +1,16 @@
 import { moment, Notice, Platform } from 'obsidian'
 import GooglePhotos from './main'
 import * as http from 'http'
-import type { IncomingMessage, ServerResponse } from 'http'
 
 export default class OAuth {
   plugin: GooglePhotos
-  port: number
+  port = 51894
   redirectUrl: string
   httpServer: http.Server
   authUrl: string
-  private readonly callbackUrl = 'https://localhost/google-photos'
 
   constructor (plugin: GooglePhotos) {
     this.plugin = plugin
-    this.port = Math.floor(Math.random() * 15000 + 50000) // random port from 50,000-65,000
     this.redirectUrl = `http://localhost:${this.port}/google-photos`
     const url = new URL('https://accounts.google.com/o/oauth2/v2/auth')
     url.search = new URLSearchParams({
@@ -49,42 +46,47 @@ export default class OAuth {
     }
     // If we can't refresh the access token, launch a full permissions request
     console.log('Google Photos: attempting permissions')
-    return this.requestPermissions()
+    this.requestPermissions()
+    // This is an asynchronous call which is picked up by the httpServer
+    // We return false here because there will no auth at this point
+    return false
   }
 
-  async requestPermissions (): Promise<boolean> {
+  requestPermissions () {
+    if (Platform.isMobile) {
+      // Electron BrowserWindow is not supported on mobile:
+      // https://github.com/obsidianmd/obsidian-releases/blob/master/plugin-review.md#nodejs-and-electron-api
+      new Notice('You will need to authenticate using a desktop device first before you can use a mobile device.')
+      return
+    }
     // Check to see if there is already a server running
     if (!this.httpServer) {
       this.httpServer = http
-        .createServer(async (req: IncomingMessage, res: ServerResponse) => {
-          console.log(req)
-          const code = new URL(req.url || '').searchParams.get('code') || ''
-          /* const tokenRes = await this.getAccessToken({
-            code,
-            client_id: this.plugin.settings.clientId,
-            client_secret: this.plugin.settings.clientSecret,
-            redirect_uri: this.callbackUrl,
-            grant_type: 'authorization_code'
-          }) */
-          const tokenRes = false
-          this.httpServer.close()
-          resolve(tokenRes)
+        .createServer(async (req, res) => {
+          if (req && req?.url?.startsWith('/google-photos')) {
+            console.log(req.url)
+            const code = new URL(this.redirectUrl + (req.url || '')).searchParams.get('code') || ''
+            const tokenRes = await this.getAccessToken({
+              code,
+              client_id: this.plugin.settings.clientId,
+              client_secret: this.plugin.settings.clientSecret,
+              redirect_uri: this.redirectUrl,
+              grant_type: 'authorization_code'
+            })
+            res.end('Authentication successful! Please return to Obsidian.')
+            this.httpServer.close()
+            return tokenRes
+          } else {
+            return false
+          }
         })
         .listen(this.port, () => {
           this.startAuthProcess()
         })
-      if (Platform.isMobile) {
-        // Electron BrowserWindow is not supported on mobile:
-        // https://github.com/obsidianmd/obsidian-releases/blob/master/plugin-review.md#nodejs-and-electron-api
-        new Notice('You will need to authenticate using a desktop device first before you can use a mobile device.')
-        resolve(false)
-      } else {
-        // Desktop devices only
-      }
+    } else {
+      // Start the auth process
+      this.startAuthProcess()
     }
-
-    // Start the auth process
-    this.startAuthProcess()
   }
 
   startAuthProcess () {
@@ -105,12 +107,12 @@ export default class OAuth {
       }
     })
     if (res.status === 200) {
-      const { access_token, refresh_token, expires_in } = await res.json()
-      this.plugin.settings.accessToken = access_token
-      if (refresh_token) {
-        this.plugin.settings.refreshToken = refresh_token
+      const tokenData = await res.json()
+      this.plugin.settings.accessToken = tokenData.access_token
+      if (tokenData.refresh_token) {
+        this.plugin.settings.refreshToken = tokenData.refresh_token
       }
-      this.plugin.settings.expires = moment().add(token.expires_in, 'second').format()
+      this.plugin.settings.expires = moment().add(tokenData.expires_in, 'second').format()
       await this.plugin.saveSettings()
       return true
     } else {
