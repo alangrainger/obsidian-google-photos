92962398ff0ad752f32d71b7c826e2f72751b548
Alan Grainger <alan.grainger@gmail.com>
2025-04-05 20:47:51 +0800

    WIP

--- src/photosApi.ts
+++ src/photosApi.ts
@@ -53,8 +53,28 @@ export type GooglePhotosMediaItem = {
   filename: string
 }
 
+type GooglePhotosPickingSession = {
+  id: string,
+  pickerUri: string,
+  expireTime: string,
+  mediaItemsSet: boolean
+}
+
+enum MediaType {
+  TYPE_UNSPECIFIED,
+  PHOTO,
+  VIDEO
+}
+
+type GooglePhotosPickedMediaItem = {
+  id: string,
+  createTime: string,
+  type: MediaType,
+  mediaFile: GooglePhotosMediaItem
+}
+
 export type GooglePhotosMediaItemsSearch = {
-  mediaItems: GooglePhotosMediaItem[],
+  mediaItems: GooglePhotosPickedMediaItem[],
   nextPageToken: string
 }
 
@@ -68,13 +88,12 @@ export default class PhotosApi {
   /**
    * Make an authenticated request to Google Photos API
    *
-   * @param {string} endpoint - Endpoint including the API version: '/v1' etc
-   * @param {object} [params] - Optional parameters
-   * @returns {Promise<object>}
+   * @param endpoint - Endpoint including the API version: '/v1' etc
+   * @param [params] - Optional parameters
    *
    * @throws Will throw an error if the input is malformed, or if the user is not authenticated
    */
-  async request (endpoint: string, params: GooglePhotosSearchParams = {}): Promise<object> {
+  async request (endpoint: string, params: RequestInit = {}) {
     // Check to make sure we have a valid access token
     const s = this.plugin.settings
     if (!s.accessToken || moment() > moment(s.expires)) {
@@ -87,7 +106,6 @@ export default class PhotosApi {
     const resp = await fetch(
       'https://photospicker.googleapis.com' + endpoint,
       Object.assign({
-        method: 'POST',
         headers: {
           Authorization: 'Bearer ' + s.accessToken,
           'Content-Type': 'application/json'
@@ -104,28 +122,35 @@ export default class PhotosApi {
         throw new Error('Unauthenticated')
       }
     } else {
+      console.log(await resp.text())
       throw new Error('Unknown status ' + resp.status)
     }
   }
 
   async createSession () {
-    return await this.request('/v1/sessions', {
-      method: 'POST'
-    })
+    const session = await this.request('/v1/sessions') as unknown as GooglePhotosPickingSession
+    // Open web picker
+    window.open(session.pickerUri, '_blank')
+    // Wait max 10 minutes for the session to complete (5 second blocks)
+    for (let i = 0; i < 120; i++) {
+      // Check to see if the session has completed
+      const res = await this.getSession(session.id)
+      if (res.mediaItemsSet) {
+        const items = await this.getMediaItems(session.id)
+        console.log(items)
+        break
+      }
+      // Wait 5 seconds
+      await new Promise(resolve => { setTimeout(resolve, 5000) })
+    }
   }
 
-  /**
-   * Perform a mediaItems search
-   * https://developers.google.com/photos/library/reference/rest/v1/mediaItems/search
-   *
-   * @param {object} options
-   * @returns {Promise<GooglePhotosMediaItemsSearch>}
-   */
-  async mediaItemsSearch (options: object = {}): Promise<GooglePhotosMediaItemsSearch> {
-    return await this.request('/v1/mediaItems:search', {
-      method: 'POST',
-      body: JSON.stringify(options)
-    }) as unknown as GooglePhotosMediaItemsSearch
+  async getSession (id: string) {
+    return await this.request('/v1/sessions/' + id, { method: 'GET' }) as unknown as GooglePhotosPickingSession
+  }
+
+  async getMediaItems (sessionId: string) {
+    return await this.request('/v1/mediaItems?sessionId=' + sessionId, { method: 'GET' }) as unknown as GooglePhotosMediaItemsSearch
   }
 
   async listAlbums (): Promise<GooglePhotosAlbumSearch> {
